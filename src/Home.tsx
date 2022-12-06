052d77eb9a92d37cfa8f534224fa3ab90367d3eb
Zachary Zhu <zach.z3394@gmail.com>
2022-12-06 18:55:45 -0500

    add panda image

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,4 @@
-import { Button, Center, HStack, Link, Text, VStack } from '@chakra-ui/react';
+import { Button, Center, HStack, Image, Link, Text, VStack } from '@chakra-ui/react';
 import ProjectCard from './ProjectCard';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 
@@ -10,61 +10,70 @@ function Home() {
       <VStack
         spacing='108px'
       >
-        <VStack
-          className='intro'
-          align='flex-start'
-          spacing='40px'
+        <HStack
+          spacing='60px'
         >
+          <Image
+            src='/cs1300-portfolio/panda.jpeg'
+            borderRadius='full'
+            boxSize='150px'
+            alt='panda image'
+          />
           <VStack
+            className='intro'
             align='flex-start'
-            spacing='24px'
+            spacing='40px'
           >
-            <Text fontSize='4xl'>
-              This is Intrepid Panda
-            </Text>
-            <Text>
-              I am a web and UX designer focused on making simple yet effective interfaces
-            </Text>
+            <VStack
+              align='flex-start'
+              spacing='24px'
+            >
+              <Text fontSize='4xl'>
+                This is Intrepid Panda
+              </Text>
+              <Text>
+                I am a web and UX designer focused on making simple yet effective interfaces
+              </Text>
+            </VStack>
+            <HStack spacing='24px'>
+              <Button
+                as={Link}
+                variant='link'
+                href='https://www.linkedin.com/in/profjeffhuang'
+                aria-label='link to LinkedIn'
+                isExternal
+              >
+                LinkedIn <ExternalLinkIcon mx='4px'/>
+              </Button>
+              <Button
+                as={Link}
+                variant='link'
+                href='https://github.com/intrepidpanda123'
+                aria-label='link to GitHub'
+                isExternal
+              >
+                GitHub <ExternalLinkIcon mx='4px' />
+              </Button>
+              <Button
+                as={Link}
+                variant='link'
+                to='/'
+                aria-label='link to resume'
+                isExternal
+              >
+                Resume
+              </Button>
+              <Button
+                as={Link}
+                variant='link'
+                href='mailto:115044343+intrepidpanda123@users.noreply.github.com'
+                aria-label='link to send email'
+              >
+                Email
+              </Button>
+            </HStack>
           </VStack>
-          <HStack spacing='24px'>
-            <Button
-              as={Link}
-              variant='link'
-              href='https://www.linkedin.com/in/profjeffhuang'
-              aria-label='link to LinkedIn'
-              isExternal
-            >
-              LinkedIn <ExternalLinkIcon mx='4px'/>
-            </Button>
-            <Button
-              as={Link}
-              variant='link'
-              href='https://github.com/intrepidpanda123'
-              aria-label='link to GitHub'
-              isExternal
-            >
-              GitHub <ExternalLinkIcon mx='4px' />
-            </Button>
-            <Button
-              as={Link}
-              variant='link'
-              to='/'
-              aria-label='link to resume'
-              isExternal
-            >
-              Resume
-            </Button>
-            <Button
-              as={Link}
-              variant='link'
-              href='mailto:115044343+intrepidpanda123@users.noreply.github.com'
-              aria-label='link to send email'
-            >
-              Email
-            </Button>
-          </HStack>
-        </VStack>
-
+        </HStack>
         <VStack id='portfolio-section' spacing='80px'>
           <Text fontSize='3xl'>
             Portfolio
@@ -84,7 +93,7 @@ function Home() {
           <VStack
             spacing='128px'
           >
-            <ProjectCard name='Ordering at Ten One Tea House' description='A brief description about personas and storyboarding' imgPath='/cs1300-portfolio/tenone.jpg' linkTo='/uiux/personas-and-storyboarding'/>
+            <ProjectCard name='Ordering at Ten One Tea House' description='A brief description about personas and storyboarding' imgPath='/cs1300-portfolio/tenone.jpg' linkTo='/uiux/personas-storyboarding'/>
           </VStack>
         </VStack>
       </VStack>
